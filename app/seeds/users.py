c495a81bafb0a8f5db67251d05f4c2e01c29fe8e
Eddie <Edwinjlau26@gmail.com>
2022-08-02 14:30:26 -0600

    docker/heroku set up

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,12 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='demo ', email='demo@email.com', password='password')
+    labbit = User(
+        username='labbit', email='xeddieee@gmail.com', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(labbit)
 
     db.session.commit()
 
