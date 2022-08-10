38a9cf4469696c82da6b5c522f3035e8cb43b7db
Eddie <Edwinjlau26@gmail.com>
2022-08-10 10:55:43 -0600

    temp

--- react-app/src/components/FetchSports/index.js
+++ react-app/src/components/FetchSports/index.js
@@ -1,33 +1,13 @@
-import { useState } from "react"
+
 
 const FetchSports = () => {
-
-    const [sports, setSports] = useState();
-
-
-    const handleClick = async () => {
-        const res = await fetch("https://api.allsportdb.com/v3/calendar?dateTo=2022-08-31&competition=formula%201", {
-            headers: {
-                Accept: "application/json",
-                Authorization: "Bearer 4732f410-c829-4f5d-9764-4b78a1a723b9"
-            }
-        })
-
-        if (res.ok) {
-            const data = await res.json();
-            setSports(data)
-        }
-    }
-
-
 
 
 
 
     return (
         <div>
-            <button onClick={handleClick}>click</button>
-            {sports && { sports }}
+Sports data
         </div>
     )
 
