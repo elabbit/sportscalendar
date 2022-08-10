3504354deba2ad3b8b01632fe526ffe11f553485
Eddie <Edwinjlau26@gmail.com>
2022-08-10 10:44:34 -0600

    test

--- react-app/src/components/FetchSports/index.js
+++ react-app/src/components/FetchSports/index.js
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 const FetchSports = () => {
 
     const [sports, setSports] = useState();
 
 
-    useEffect = () => {
-        const getSports = () => async () => {
-            const res = await fetch("https://api.allsportdb.com/v3/calendar?dateTo=2022-08-31&competition=formula%201", {
-                headers: {
-                    Accept: "application/json",
-                    Authorization: "Bearer 4732f410-c829-4f5d-9764-4b78a1a723b9"
-                }
-            })
-
-            if (res.ok) {
-                const data = await res.json();
-                setSports(data)
+    const handleClick = async () => {
+        const res = await fetch("https://api.allsportdb.com/v3/calendar?dateTo=2022-08-31&competition=formula%201", {
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer 4732f410-c829-4f5d-9764-4b78a1a723b9"
             }
+        })
+
+        if (res.ok) {
+            const data = await res.json();
+            setSports(data)
         }
-        getSports()
     }
+
+
+
 
 
 
     return (
         <div>
+            <button onClick={handleClick}>click</button>
             {sports && { sports }}
         </div>
     )
