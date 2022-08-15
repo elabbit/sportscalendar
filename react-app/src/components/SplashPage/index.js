d8bd63cdf9999889a38df2ecf118e690a3a8f4ba
Eddie <Edwinjlau26@gmail.com>
2022-08-15 00:41:42 -0600

    work in progress

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,25 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoginForm from "../auth/LoginForm";
 import SignUpForm from "../auth/SignUpForm";
 import "./SplashPage.css"
 import splash from "../../images/splash-temp.png"
+import splashImg1 from '../../images/realsplash1.png'
+import splashImg2 from '../../images/realsplash2.png'
+import splashImg3 from '../../images/realsplash3.png'
 
 const SplashPage = () => {
     const [showLogin, setShowLogin] = useState(true);
+    const [indexOne, setIndexOne] = useState(0);
+    const [indexTwo, setIndexTwo] = useState(1);
+    const [indexThree, setIndexThree] = useState(2);
+
+    const splashImages = [splashImg1, splashImg2, splashImg3]
+    const classes = ["splash-img fade-in", "splash-img ade-out", "splash-img fade-none"]
+
+    useEffect(()=> {
+      const imgId = setInterval(()=> {
+        if(indexOne === 2) {
+          setIndexOne(0);
+        }
+        else {
+          setIndexOne(indexOne + 1)
+        }
+
+        if(indexTwo === 2) {
+          setIndexTwo(0);
+        }
+        else {
+          setIndexTwo(indexTwo + 1)
+        }
+
+        if(indexThree === 2) {
+          setIndexThree(0);
+        }
+        else {
+          setIndexThree(indexThree + 1)
+        }
+
+      }, 5000)
+      return () => clearInterval(imgId);
+    }, [splashImages.length, indexOne, indexTwo, indexThree]);
+
 
     return (
         <div className='splash-outer'>
             <div className="splash-container">
                 <div>
                     <img src={splash} alt=''></img>
-                </div>
-                <div className='splash-form'>
+                    </div>
+                    {/* <div className="splash-img-container">
+                        <img className={`${classes[indexOne]}`} src={splashImages[0]} alt="" />
+                        <img className={`${classes[indexTwo]}`} src={splashImages[1]} alt="" />
+                        <img className={`${classes[indexThree]}`} src={splashImages[2]} alt="" />
+                    </div> */}
+            </div>
+            <div className='splash-form'>
                 {showLogin ?
-                    <LoginForm setShowLogin={setShowLogin}/>
+                    <LoginForm setShowLogin={setShowLogin} />
                     :
-                    <SignUpForm setShowLogin={setShowLogin}/>
+                    <SignUpForm setShowLogin={setShowLogin} />
                 }
-                </div>
             </div>
         </div>
     )
