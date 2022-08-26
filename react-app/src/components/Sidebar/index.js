ea97e6b08f7ffc901bafb9e97abc468f3233c40a
Eddie <Edwinjlau26@gmail.com>
2022-08-26 02:49:37 -0600

    about clear current

--- react-app/src/components/Sidebar/index.js
+++ react-app/src/components/Sidebar/index.js
@@ -8,12 +8,17 @@ import ListCategory from "../ListCategory/index.js"
 import AddEventModal from "../AddEventModal"
 
 const Sidebar = ({ right, setRight, setShowCal, showEditEvent, setShowEditEvent}) => {
-    const { setShowSide, setCurrentEvent, setCurrCat} = useContext(CalendarContext)
+    const { setShowSide, setCurrentEvent, setCurrCat, setCurrentCalendar} = useContext(CalendarContext)
 
     const handleSideCancel = () =>{
         setShowSide(false)
         setCurrentEvent('')
         setCurrCat('all')
+    }
+
+    const handleAbout = () => {
+       setShowCal(false)
+       setCurrentCalendar('')
     }
 
     return (
@@ -31,7 +36,7 @@ const Sidebar = ({ right, setRight, setShowCal, showEditEvent, setShowEditEvent}
             <div className="sidebar-navbar">
                 <i className="fa-solid fa-right-left" onClick={() => setRight(!right)}></i>
                 <i className="fa-solid fa-house" onClick={() => setShowCal(true)}></i>
-                <i className="fa-solid fa-circle-info" onClick={() => setShowCal(false)}></i>
+                <i className="fa-solid fa-circle-info" onClick={handleAbout}></i>
                 <LogoutButton />
             </div>
             <div className="sidebar-options">
